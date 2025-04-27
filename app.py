35662eb4e97b9e87dd3a3ec239aaa7bff5fc679c
Abhishek Kumar <abhishek.kumar11101gmail.com>
2025-04-27 10:09:39 +0100

    ccreated Database and popup data of todo

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -15,12 +16,23 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"<Todo {self.title} - {self.Sno}>"
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
-@app.route("/products")
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+    todo = Todo(title = title, desc = desc) ## create an object of the Todo class
+    db.session.add(todo) ## add the object to the session
+    db.session.commit() ## commit the session to the database
+    all_todo = Todo.query.all()
+    return render_template("index.html", all_todo=all_todo) ## render the template with the objects
+@app.route("/show")
 def products():
+    all_todo = Todo.query.all() ## query all the objects of the Todo class
+    print(all_todo) ## print the objects in the console
     return "<p>New products</p>"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=8000)
